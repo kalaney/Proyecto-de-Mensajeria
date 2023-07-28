07f7699eaf8d4faf4823ac0d7394f065670857d6
kalaney <ksolano90876@ufide.ac.cr>
2023-07-28 01:47:09 -0600

    Create Avance de Kalaney.py

---
+++ Avance de Kalaney.py
@@ -0,0 +1,13 @@
+import random
+import datetime
+#INFORMACION COMERCIAL/Nombre/Telefono
+print("Guia del Paquete:""\n\n""Remitente""\n" "Mensajeria Fidelitas""\n""22656454""\n""Monto total:",suma)
+
+fechaEntraga=datetime.datetime.now()
+print("Fecha de Remitente:",fechaEntraga)
+
+
+#Numero random
+numeroGuia=random.randint(1,1000000)
+print("Numero Guia:",numeroGuia)
+print("\n")
